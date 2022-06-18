442a605fbcbdd4e2152550d874c180f6ea5aaa69
Aysel552 <a.abbasaliyeva1992@gmail.com>
2022-06-18 22:42:39 +0400

    revising javascript course

---
+++ js-week1/script.js
@@ -0,0 +1,57 @@
+// alert("This is my first piece of code in JavaScript")
+
+// let books = 7;
+// alert(books);
+
+// let country = "Italy";
+// alert(country)
+
+// let date = "Sam";
+// console.log(typeof date);
+
+// let tomatoes = 4;
+// let cucumbers = 3;
+// let milk = 6;
+// let cheese = 8;
+// let wine = 25;
+
+// let products = tomatoes + cucumbers + milk + cheese + wine
+// console.log(products);
+
+let salary = 100000
+let federalTax = 12741;
+let provincialTax = 15557;
+let otherTaxes = 3631;
+
+let money = salary - (federalTax + provincialTax + otherTaxes);
+console.log(money);
+
+
+let oranges = 10;
+let apples = 5;
+let tomatoes = 3;
+
+let message = `You bought ${oranges} oranges, ${apples} apples and ${tomatoes} tomatoes.`
+alert(message);
+
+let groceries = 100;
+let clothes = 30;
+let toys = 20;
+let expense = groceries + clothes + toys;
+
+let phrase = `You spent ${expense}$`
+alert(phrase);
+
+
+// let box = 'toys';
+// let res = box.toUpperCase()
+// console.log(res);
+
+let box = 'TOYS';
+let res = box.toLowerCase()
+console.log(res);
+
+let text = 'Hello';
+let user = "Sam";
+let result = text.concat(" ", user);
+console.log(result);
